8852ac1b7e89f05905dab4c289731cd4dcfbf8ff
Francisco Arroyo <fran.arroyo596@gmail.com>
2018-01-18 11:39:48 -0600

    npm start script updated with Budo

---
+++ index.js
@@ -0,0 +1,5 @@
+import uuidv1 from 'uuid'
+//TODO APP
+document.addEventListener("DOMContentLoaded", function() {
+console.log(uuidv1());
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import uuidv1 from 'uuid'
 //TODO APP
 document.addEventListener("DOMContentLoaded", function() {
 
